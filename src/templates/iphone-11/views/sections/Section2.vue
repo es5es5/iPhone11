9dc98dbd8ceb52f3a3df1301b3dfe5bd8279f4e3
es5es5 <es5e5s@nate.com>
2019-11-03 21:58:01 +0900

    side color label

--- src/templates/iphone-11/views/sections/Section2.vue
+++ src/templates/iphone-11/views/sections/Section2.vue
@@ -6,12 +6,14 @@
     <div class="img--sideColor__wrap">
       <img :src="require(`@/assets/img/iphone/sideColor/${selectColor}.jpg`)" class="img--sideColor" :alt="`iphone11_side_color_${selectColor}`" :title="`iphone11_side_color_${selectColor}`">
     </div>
-    <ul class="radio--sideColor_wrap">
-      <li v-for="(item, index) in colorArr" :key="index">
-        <input type="radio" name="sideColor" :id="`sideColor_${item}`" :value="item" v-model="selectColor">
-        <label :for="`sideColor_${item}`">{{ item }}</label>
-      </li>
-    </ul>
+    <div class="action_wrap">
+      <ul class="radio--sideColor_container">
+        <li class="radio--sideColor_wrap" v-for="(item, index) in colorArr" :key="index">
+          <input type="radio" name="sideColor" :id="`sideColor_${item.id}`" class="radio--sideColor" :value="item.id" v-model="selectColor">
+          <label :for="`sideColor_${item.id}`" class="label--sideColor">{{ item.name }}</label>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -20,13 +22,20 @@ export default {
   name: 'Section2',
   data () {
     return {
-      colorArr: [
-        'black',
-        'green',
-        'red',
-        'yellow'
-      ],
-      selectColor: 'red'
+      selectColor: 'red',
+      colorArr: [{
+        id: 'black',
+        name: '블랙'
+      }, {
+        id: 'green',
+        name: '그린'
+      }, {
+        id: 'red',
+        name: '레드'
+      }, {
+        id: 'yellow',
+        name: '옐로'
+      }]
     }
   }
 }
@@ -43,7 +52,30 @@ export default {
 
   .img--sideColor {
     width: 90%;
-    height: 50%;
+  }
+}
+
+.action_wrap {
+  width: 80%;
+  margin: 0 auto;
+
+  .radio--sideColor_container {
+    .radio--sideColor_wrap {
+      position: relative;
+      display: inline-block;
+      padding: 1rem;
+
+      .radio--sideColor {
+        // go ahead.
+      }
+      .label--sideColor {
+        position: absolute;
+        bottom: 3rem;
+        left: .5rem;
+        font-weight: 600;
+        font-size: 1.2rem;
+      }
+    }
   }
 }
 
