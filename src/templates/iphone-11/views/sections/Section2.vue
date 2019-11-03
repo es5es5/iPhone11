4d216131d98e339bcb4a109814be2239e743b458
es5es5 <es5e5s@nate.com>
2019-11-03 21:04:01 +0900

    side color

--- src/templates/iphone-11/views/sections/Section2.vue
+++ src/templates/iphone-11/views/sections/Section2.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="section2">
     <div class="contents_wrap">
-      <div class="slide_warp">
-        <h2 class="section--title">첫눈에 반하다. 한 번, 두 번, 세 번, 네 번, 다섯 번, 여섯 번이나.</h2>
-        <img src="" alt="iPhone 11 [CHOOSE THE COLOR Image] Here." title="iphone11_side">
-      </div>
+      <h2 class="section--title">첫눈에 반하다. 한 번, 두 번, 세 번, 네 번, 다섯 번, 여섯 번이나.</h2>
     </div>
+    <div class="img--sideColor__wrap">
+      <img :src="require(`@/assets/img/iphone/sideColor/${selectColor}.jpg`)" class="img--sideColor" :alt="`iphone11_side_color_${selectColor}`" :title="`iphone11_side_color_${selectColor}`">
+    </div>
+    <ul class="radio--sideColor_wrap">
+      <li v-for="(item, index) in colorArr" :key="index">
+        <input type="radio" name="sideColor" :id="`sideColor_${item}`" :value="item" v-model="selectColor">
+        <label :for="`sideColor_${item}`">{{ item }}</label>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Section2'
+  name: 'Section2',
+  data () {
+    return {
+      colorArr: [
+        'black',
+        'green',
+        'red',
+        'yellow'
+      ],
+      selectColor: 'red'
+    }
+  }
 }
 </script>
 
@@ -19,7 +36,15 @@ export default {
 .vod_wrap {
   font-size: 1.715rem;
   font-weight: 600;
+}
 
+.img--sideColor__wrap {
+  text-align: right;
+
+  .img--sideColor {
+    width: 90%;
+    height: 50%;
+  }
 }
 
 </style>
