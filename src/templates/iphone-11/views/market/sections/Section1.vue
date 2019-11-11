227e868cc7b9d98c1908fb90dc2170f78aedf425
es5es5 <es5e5s@nate.com>
2019-11-12 00:50:56 +0900

    market color

--- src/templates/iphone-11/views/market/sections/Section1.vue
+++ src/templates/iphone-11/views/market/sections/Section1.vue
@@ -7,6 +7,12 @@
       <h3>iPhone11 구입하기</h3>
       <p>₩990,000 부터</p>
       <p>맘에 드는 마감을 선택하세요.</p>
+      <div class="color_wrap">
+        <div class="color" v-for="(item, index) in colorArr" :key="`color_${index}`">
+          <div class="colorBall" :style="`background-color: ${item.id}`"></div>
+          <span class="colorName">{{ item.name }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,11 +29,17 @@ export default {
         id: 'green',
         name: '그린'
       }, {
-        id: 'red',
-        name: '레드'
+        id: 'purple',
+        name: '퍼플'
       }, {
         id: 'yellow',
         name: '옐로'
+      }, {
+        id: 'white',
+        name: '화이트'
+      }, {
+        id: 'red',
+        name: '레드'
       }]
     }
   }
@@ -50,5 +62,32 @@ export default {
 .right_wrap {
   width: 50%;
   float: right;
+}
+
+.color_wrap {
+  width: 100%;
+
+  .color {
+    display: inline-block;
+    text-align: center;
+    cursor: pointer;
+    box-sizing: border-box;
+    width: calc(50% - 1rem);
+    padding-top: 15%;
+    border: 1px solid #ddd;
+    margin: .5rem;
+
+    &:active {
+      box-sizing: border-box;      
+      border: 2px solid blue;
+    }
+  }
+  .colorBall {
+    margin: 0 auto;
+    width: 2.5rem;
+    height: 2.5rem;
+    border: 1px solid #ccc;
+    border-radius: 2.5rem;
+  }
 }
 </style>>
