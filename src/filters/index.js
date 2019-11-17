99f9d36e7b1a238e186fe803997fb04e242db3c3
es5es5 <es5e5s@nate.com>
2019-11-18 00:14:50 +0900

    filter

---
+++ src/filters/index.js
@@ -0,0 +1,16 @@
+export default {
+  install (Vue) {
+    /**
+     * 숫자, 천 단위마다 콤마 필터
+     */
+    Vue.filter('numberWithComma', value => {
+      if (value !== 0 && !value) return ''
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    })
+
+    Vue.filter('numberWithCommaDollar', value => {
+      if (value !== 0 && !value) return ''
+      return `$${value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import GlobalComponents from './templates/iphone-11/components'
+import GlobalFilters from './filters'
 
 import './plugins/vue-awesome'
 
 Vue.use(GlobalComponents)
+Vue.use(GlobalFilters)
 Vue.config.productionTip = false
 
 new Vue({
