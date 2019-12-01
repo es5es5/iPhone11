8160b2134c48b2f83c573ba92c4eee92cc0c61c6
es5es5 <es5e5s@nate.com>
2019-12-01 14:59:05 +0900

    Home link

--- src/templates/Home.vue
+++ src/templates/Home.vue
@@ -4,14 +4,15 @@
       <h1>Hello! Welcome to My Page.</h1>
       <p>This is just a copy for Study.</p>
       <br>
-      <router-link :to="{ name: 'IPhone11' }">go IPhone11</router-link>
+      <router-link tag="a" class="link" :to="{ name: 'IPhone11' }">iPhone11.</router-link>
+      <router-link tag="a" class="link" :to="{ name: 'IPhoneMarketLayout' }">iPhone11 Market.</router-link>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Home'
 }
 </script>
 
@@ -19,5 +20,9 @@ export default {
 .welcome_wrap {
   margin: 10%;
   text-align: center;
+  line-height: 1.5;
+}
+a {
+  display: block;
 }
 </style>
