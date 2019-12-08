be8aa796ad36fa03b304919d534f02e9b91828d9
es5es5 <es5e5s@nate.com>
2019-12-08 23:36:55 +0900

    market section2

---
+++ src/templates/iphone-11/views/market/sections/Section2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="section2">
+    <!-- 슬라이더로 구현 예정 -->
+    <div class="gallery_wrap">
+      <img class="gallery" src="@/assets/img/iphone/market/gallery_1.jpeg" alt="gallery_1">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Section2'
+}
+</script>
+
+<style lang="scss" scoped>
+.gallery {
+  width: 100%;
+  height: auto;
+}
+</style>
