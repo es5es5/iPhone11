09c36a6d14f6fd5feebb82cf039be3c6a825e63a
jsg3121 <xodm95@gmail.com>
2019-12-27 17:46:56 +0900

    2019.12.27_service_section1_content_build

--- src/templates/iphone-11/views/serviceCenter/sections/Section1.vue
+++ src/templates/iphone-11/views/serviceCenter/sections/Section1.vue
@@ -1,5 +1,13 @@
 <template>
     <div class = "section1 clearfix">
-        jhgf
+        <div class = "img_wrap">
+            <div class = "wrap_innerContent">
+                <p class = "title">Apple 지원에 오신것을 환영합니다</p>
+                <div class = "searchBox">
+                    <img alt = "검색" class="btn_search_img" src="@/assets/img/iphone/service/search_icon_black.png">
+                    <input v-model="message" placeholder="무엇이 궁금한가요?" autocomplete="none">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
